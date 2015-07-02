69d8b9e74230928d8063a487e836169e63ed3086
office <chinalihongde@163.com>
2015-07-02 09:08:03 +0800

    some modules use php to realize

--- others.php
+++ others.php
@@ -46,4 +46,23 @@ $treeList=list_to_tree($list);
 echo('<pre>');
 print_r($treeList);
 echo('</pre>');
+
+
+
+
+
+function object_to_array($obj)
+{
+    $_arr = is_object($obj) ? get_object_vars($obj) : $obj;
+    $arr = array();
+    if (!empty($_arr)) {
+        foreach ($_arr as $key => $val) {
+            $val = (is_array($val)) || is_object($val) ? object_to_array($val) : $val;
+            $arr[$key] = $val;
+        }
+    }
+
+    return $arr;
+
+}
 ?>
