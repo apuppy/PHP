93d1fe33f26ec6d4ddb26b65d3eb5a6a2b430dfe
kevin <chinalihongde@163.com>
2015-12-01 19:12:00 +0800

    set default value true to terminate the program bellow in Helper debug method

--- Helpers/Helper.php
+++ Helpers/Helper.php
@@ -14,7 +14,7 @@ class Helper{
      * @param mixed 要打印的变量名
      * @param boolean 是否调用exit阻止向下执行的标志
      */
-    public static function vd($var,$exitFlag = false){
+    public static function vd($var,$exitFlag = true){
         echo '<pre>';
         var_dump($var);
         echo '</pre>';
@@ -29,7 +29,7 @@ class Helper{
      * @param mixed 要打印的变量名
      * @param boolean 是否调用exit阻止向下执行的标志
      */
-    function vp($var,$exitFlag = false){
+    function vp($var,$exitFlag = true){
         echo '<pre>';
         print_r($var);
         echo '</pre>';
