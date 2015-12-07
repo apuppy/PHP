61fdc701a900a9c682132f0fc7b1dcc93a801fb7
kevin <chinalihongde@163.com>
2015-12-07 19:00:33 +0800

    remove oop style

--- Helpers/Helper.php
+++ Helpers/Helper.php
@@ -5,16 +5,14 @@
  * Time: 16:14
  * 公共帮助类
  */
-namespace Helpers;
-
-class Helper{
 
     /**
      * @function
      * @param mixed 要打印的变量名
      * @param boolean 是否调用exit阻止向下执行的标志
      */
-    public static function vd($var,$exitFlag = true){
+if(!function_exists('vd')){
+    function vd($var,$exitFlag = true){
         echo '<pre>';
         var_dump($var);
         echo '</pre>';
@@ -23,12 +21,14 @@ class Helper{
             exit();
         }
     }
+}
 
     /**
      * @function
      * @param mixed 要打印的变量名
      * @param boolean 是否调用exit阻止向下执行的标志
      */
+if(!function_exists('vd')){
     function vp($var,$exitFlag = true){
         echo '<pre>';
         print_r($var);
@@ -38,7 +38,6 @@ class Helper{
             exit();
         }
     }
+}
 
     //TODO var_export 输出或返回一个变量的字符串表示
-
-}
